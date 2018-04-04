abd71c4a7a9512a66540fe692a59c7fb97a5f219
varnikapounraj <36755184+varnikapounraj@users.noreply.github.com>
2018-04-04 04:58:17 +0000

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+double number;
+printf("enter the number");
+scanf("%lf",&Number);
+if(Number <=0.0)
+{
+if(number==0.0)
+printf("your entered number is 0.");
+else
+printf("your entered number is negative");
+}
+else
+printf("your entered number is positive");
+return 0;
+}
