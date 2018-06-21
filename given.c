a2d2d72b46dcb0fde17aa16aee22ae58ee69969f
varnikapounraj <36755184+varnikapounraj@users.noreply.github.com>
2018-06-21 12:35:48 +0000

    Create given.c

---
+++ given.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        longint sum=0,i,n;
+        clrscr();
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+        getch();
+        }
