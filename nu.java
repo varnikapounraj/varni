4c40c57cab4b455a4c01536fe2932a598a8e7132
varnikapounraj <36755184+varnikapounraj@users.noreply.github.com>
2018-06-23 06:02:45 +0000

    Create nu.java

---
+++ nu.java
@@ -0,0 +1,24 @@
+public static boolean isStringNumeric( String str )
+{
+    DecimalFormatSymbols currentLocaleSymbols = DecimalFormatSymbols.getInstance();
+    char localeMinusSign = currentLocaleSymbols.getMinusSign();
+
+    if ( !Character.isDigit( str.charAt( 0 ) ) && str.charAt( 0 ) != localeMinusSign ) return false;
+
+    boolean isDecimalSeparatorFound = false;
+    char localeDecimalSeparator = currentLocaleSymbols.getDecimalSeparator();
+
+    for ( char c : str.substring( 1 ).toCharArray() )
+    {
+        if ( !Character.isDigit( c ) )
+        {
+            if ( c == localeDecimalSeparator && !isDecimalSeparatorFound )
+            {
+                isDecimalSeparatorFound = true;
+                continue;
+            }
+            return false;
+        }
+    }
+    return true;
+}
