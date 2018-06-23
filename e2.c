f276aec040a9c63ca73d8189f965830ef5399103
varnikapounraj <36755184+varnikapounraj@users.noreply.github.com>
2018-06-23 05:32:46 +0000

    Create e2.c

---
+++ e2.c
@@ -0,0 +1,75 @@
+37
+	#include <iostream>
+  using namespace std;
+  void getDuplicate(int arr[],int size)
+  {
+
+	    int pos = 0;
+
+	    int desiredPos = 0;
+
+	    while(pos < size)
+
+	    {
+
+	        if(arr[pos] <= 0)
+
+	        {
+          pos++;
+	            continue;
+
+	        }
+
+	        desiredPos = arr[pos] -1;
+
+	        if(arr[desiredPos] > 0)
+
+	        {
+
+	            arr[pos] = arr[desiredPos];
+
+	            arr[desiredPos] = -1;
+
+	        }
+
+	        else
+
+	        {
+
+	            arr[desiredPos]--;
+
+	            arr[pos] = 0;
+
+	            pos++;
+
+	        }
+
+	    }
+
+	}
+
+	  
+
+	void display(int arr[], int size)
+
+	{
+
+	    for(int i=0; i<size; i++)
+
+	        cout<<"\nElement = "<<i+1<<"\tFrequency = "<<arr[i]*-1;
+
+	}
+
+	  
+
+	int main()
+
+	{
+
+	    int arr[] = {6,4,1,4,3,2,5,2,1};
+	    int size = sizeof(arr)/sizeof(arr[0]);
+	    getDuplicate(arr,size);
+
+	    display(arr,size);
+
+	}
