bec721b0f16fc9d14f8995361299151e77bce579
varnikapounraj <36755184+varnikapounraj@users.noreply.github.com>
2018-07-09 14:49:23 +0530

    Create Id.java

---
+++ Id.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Id
+{
+public static void main(String args[]) throws java.lang.Exception
+{
+Scannersc=new Scanner(System.in);
+String str=s.nextLine();
+String str1=str.replaceAll(" "," ");
+int len=str1.length();
+System.out.println(len);
+}
+}
